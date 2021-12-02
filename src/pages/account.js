ce475bb05e7753bca0b43c40fb3554936e9215eb
Sohaib Tariq <sohaibtariq12@gmail.com>
2021-12-02 16:59:43 +0500

    added portal

--- src/pages/account.js
+++ src/pages/account.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Router } from "@reach/router"
 import { login, logout, isAuthenticated, getProfile } from "../utils/auth"
 import { Link } from "gatsby"
+import { Payments } from "./balance-api"
 
 const Home = ({ user }) => {
   return <p>Hi, {user.name ? user.name : "friend"}!</p>
@@ -16,13 +17,22 @@ const Account = () => {
   }
 
   const user = getProfile()
-
+  const apimaticScript = require("./apimatic.js")
   return (
     <>
       <nav>
         <Link to="/account">Home</Link>{" "}
         <Link to="/account/settings">Settings</Link>{" "}
         <Link to="/account/billing">Billing</Link>{" "}
+        <Link
+          to="/balance-api"
+          className="w-1/2 h-48 max-w-lg px-5 py-4 mt-16 bg-white border rounded-lg cursor-pointer hover:shadow-lg border-gray"
+        >
+          <div className="text-4xl font-medium text-gray-700">volume-api</div>
+          {/* <p className="h-48 mt-4 text-base leading-tight text-gray-700">
+            Some text about the api
+          </p> */}
+        </Link>
         <a
           href="#logout"
           onClick={e => {
@@ -33,6 +43,7 @@ const Account = () => {
           Log Out
         </a>
       </nav>
+
       <Router>
         <Home path="/account" user={user} />
         <Settings path="/account/settings" />
